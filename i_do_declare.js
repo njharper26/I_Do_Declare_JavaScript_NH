134735e3f01337a088fa3356becdff9af3b2f28e
njharper26 <njharper@gmail.com>
2017-07-24 08:57:11 -0500

    initial commit

---
+++ i_do_declare.js
@@ -0,0 +1,23 @@
+var num1 = 1;
+var num2 = 100;
+var num3 = 1.001;
+var num4 = -23456;
+
+var string1 = 'Coding Dojo';
+var string2 = '213 W. Institute Place';
+var string3 = 'JavaScript Basics';
+
+var todayIsMon = true;
+var isWeekend = false;
+
+var undef;
+
+console.log('First Number: ', num1)
+console.log('Second Number: ', num2)
+console.log('Third Number: ', num3)
+console.log('Fourth Number: ', num4)
+console.log('First String: ', string1)
+console.log('Second String: ', string2)
+console.log('First Boolean: ', todayIsMon)
+console.log('Second Boolean: ', isWeekend)
+console.log('undef: ', undef)
